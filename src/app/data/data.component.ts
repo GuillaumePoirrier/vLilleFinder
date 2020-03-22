c30425e78f813f16c6fb9d676276c8851160b2c3
GuillaumePoirrier <guillaume.poirrier@hei.yncrea.fr>
2020-03-22 20:52:02 +0100

    third

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { generateExpandoInstructionBlock } from '@angular/core/src/render3/instructions';
+
 
 
 
